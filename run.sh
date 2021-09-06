0adb5d84bdfe184b5f26eab939fc3500d825d71a
nosami <jasonimison@gmail.com>
2021-09-06 11:48:52 +0100

    remove dupe

--- run.sh
+++ run.sh
@@ -3,4 +3,3 @@ ln -s ~/src/configs/.zshrc ~/.zshrc
 ln -s ~/src/configs/.gitconfig ~/.gitconfig
 ln -s ~/src/configs/.vsvimrc ~/.vsvimrc
 ln -s ~/src/configs/.vimrc ~/.vimrc
-ln -s ~/src/configs/.gitconfig ~/.gitconfig
