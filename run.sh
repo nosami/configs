f9f0f7528d1a9462bb114306775e8080de0c9902
nosami <jasonimison@gmail.com>
2021-09-06 11:47:18 +0100

    Add ln script

---
+++ run.sh
@@ -0,0 +1,6 @@
+ln -s ~/src/configs/.spacemacs ~/.spacemacs
+ln -s ~/src/configs/.zshrc ~/.zshrc
+ln -s ~/src/configs/.gitconfig ~/.gitconfig
+ln -s ~/src/configs/.vsvimrc ~/.vsvimrc
+ln -s ~/src/configs/.vimrc ~/.vimrc
+ln -s ~/src/configs/.gitconfig ~/.gitconfig
