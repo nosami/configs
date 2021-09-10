4a19b57e73e67c0fb5bf2aa2ed68ffaebf97cd3b
nosami <jasonimison@gmail.com>
2021-09-10 10:53:33 +0100

    Add .ssh folder

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ ln -s ~/src/configs/.zshrc ~/.zshrc
 ln -s ~/src/configs/.gitconfig ~/.gitconfig
 ln -s ~/src/configs/.vsvimrc ~/.vsvimrc
 ln -s ~/src/configs/.vimrc ~/.vimrc
+mkdir -p ~/.ssh
 ln -s ~/src/configs/sshconfig ~/.ssh/config
